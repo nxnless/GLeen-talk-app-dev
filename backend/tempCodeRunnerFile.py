f8b1f3c4de33575a812dec10fcdb2c4b3dee9e57
nxnless <koonnon3453@gmail.com>
2024-03-30 11:39:20 +0700

    Restart

--- backend/tempCodeRunnerFile.py
+++ backend/tempCodeRunnerFile.py
@@ -1,13 +1,1 @@
-@app.route('/api/PostByUserID/<string:user_id>', methods=['GET'])
-# @cross_origin()
-# def get_PostByUserID(user_id):
-#     try:
-#         client.admin.command("ping")
-#         db = client["AppDev"]
-#         collection = db["Post"]
-        
-#         posts = list(collection.find({"User_ID": user_id}).sort("_id", -1))
-        
-#         return jsonify(posts), 200
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
+ents = list(coll
